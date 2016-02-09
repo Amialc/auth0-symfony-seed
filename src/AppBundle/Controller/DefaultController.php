43173a47e90dd71a4c7c7e81c6eabc5a88ba333f
Vlad <pleo12@gmail.com>
2016-02-09 11:11:04 +0200

    Delete credentials, controlled now passing credentials from .env file

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Dotenv\Dotenv;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,13 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('index.html.twig');
+        $dotenv = new Dotenv(__DIR__ . '/../../..');
+        $dotenv->load();
+        return $this->render('index.html.twig', array(
+            'AUTH0_DOMAIN' => getenv('AUTH0_DOMAIN'),
+            'AUTH0_CLIENT_ID' => getenv('AUTH0_CLIENT_ID'),
+            'AUTH0_CLIENT_SECRET' => getenv('AUTH0_CLIENT_SECRET')
+        ));
     }
 }
 
